b8da063d58f261b38764cb3c0f8c9f3b92079473
Ali Raza <aliraza.2369196@gmail.com>
2024-06-12 18:16:27 +0500

    setup vue, tailwind css and inertia

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.min.js"></script>
+</body>
+
+</html>
